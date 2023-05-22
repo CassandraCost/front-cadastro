3f2aa4c85bfffe16dde5004ab1ff2263a68bab77
CassandraCost <cassandra.costa10@gmail.com>
2023-05-22 13:49:53 -0300

    Iniciando tela de cadastro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import './styles.css'
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [nome, setNome] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='container-cadastro'>
+        <div className='bloco-cadastro'>
+          <div className='avatar-usuario'></div>
+
+          <div className='texto'>
+
+            <div className='texto-esc1'>
+            <input className= { (email.length > 0 ? 'has-val ' : '') + 'input'} 
+              type='nome'
+              value={nome}
+              onChange={e=>setNome(e.target.value)}/>
+              <span className='focus-input' data-placeholder='Nome'></span>
+            </div>
+
+            <div className='texto-esc1'>
+            <input className= { (email.length > 0 ? 'has-val ' : '') + 'input'} 
+              type='email'
+              value={email}
+              onChange={e=>setEmail(e.target.value)}/>
+              <span className='focus-input' data-placeholder='Email'></span>
+            </div>
+
+            <div className='texto-esc1'>
+              <input className= { (senha.length > 0 ? 'has-val ' : '') + 'input'} 
+              type='password'
+              value={senha}
+              onChange={e=>setSenha(e.target.value)}/>
+              <span className='focus-input' data-placeholder='Senha'></span>
+            </div>
+
+            <div className='btn-entar'>
+              <button className='botao-entrar'>Finalizar</button> 
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
